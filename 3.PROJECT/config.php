28107e0c7038c6fa5bac18b56564e004eb416b40
KimCuc273 <you@example.com>
2020-11-03 16:28:08 +0700

    update project

--- 3.PROJECT/config.php
+++ 3.PROJECT/config.php
@@ -2,7 +2,7 @@
     $host = 'localhost';
     $user = 'root';
     $pass = '';
-    $db   = 'project';
+    $db   = 'giangvien';
     $conn = mysqli_connect($host,$user,$pass, $db);
     if(!$conn){
         die("Không thể kết nối");
