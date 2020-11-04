78127240a99e777f074eabb13eadabdae42aab2b
PhanThiQuyen144 <67349994+PhanThiQuyen144@users.noreply.github.com>
2020-11-05 00:24:10 +0700

    upload files

---
+++ 3.PROJECT/include/gioithieu/hoptacquocte.php
@@ -0,0 +1,204 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Lễ ký kết thỏa thuận hợp tác</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+     <link rel="stylesheet" href="../../CSE.css">
+     <link rel="stylesheet" href="../../css/logo.css">
+  </head>
+  <body>
+    <form action="" method="post">
+        <?php 
+            include("../../header.php");
+        ?>
+            <div class="main ">
+                <div class="container  "  >
+                    <div class="row ">
+                        <div class="col-md-12 gthieu text-white">
+                            <h5 >
+                                <a href="../gioithieu/gioithieu.php" class="text-white">Giới thiệu</a> > <a href="../gioithieu/hoptacquocte.php" class="text-white">Hợp tác liên kết</a> 
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="row content">
+                        <div class="col-md-9 logo">
+                            <div class="col-md-12">
+                                <p align="justify" >Vào ngày  20/01/2016 tại trường Đại học Thủy Lợi
+                                     đã diễn ra lễ kí kết hợp tác đào tạo với Công ty Fsoft – FPT.</p>
+                                <p align="justify">
+                                    Tham dự buổi lễ có GS.TS Nguyễn Quang Kim - Bí thư Đảng Ủy, Hiệu trưởng Nhà trường; 
+                                    TS Nguyễn Thanh Tùng -Trưởng khoa CNTT; Ông Trần Xuân Khôi - Phó tổng giám đốc công ty FSOFT 
+                                    cùng các giảng viên và sinh viên khoa Công nghệ thông tin- Trường Đại học Thủy Lợi.
+                                </p>
+                                <p align="justify">
+                                    Tại buổi kí kết,  GS.TS Nguyễn Quang Kim đại diện Trường Đại học Thủy Lợi giới thiệu về nhà trường 
+                                    với công ty FSOFT:  về quy mô, cơ sơ vật chất, các lĩnh vực đào tạo, đồng thời  nói lên tầm quan trọng 
+                                     của việc hợp tác và định hướng nghề nghiệp cho các sinh viên. Nhà trường rất vui mừng và ủng hộ, tạo
+                                      điều kiện cho những chương trình thiết thực như vậy.
+                                </p>
+                                <img src="../../images/cntt ky ket 1.jpg" alt="">
+                                <p align="justify">
+                                    TS Nguyễn Thanh Tùng - Trưởng khoa Công nghệ thông tin (CNTT)
+                                     phát biểu: “Việc ký kết hợp tác với các doanh nghiệp sẽ không chỉ 
+                                     đưa doanh nghiệp tham gia vào quá trình đào tạo mà còn đem lại nhiều
+                                      cơ hội cho các sinh viên khoa CNTT.” 
+                                </p>
+                                <p align="justify">
+                                    Tiếp đó, Ông Trần Xuân Khôi - Phó tổng giám đốc công ty FSOFT bày tỏ sự vui mừng khi được tham gia hợp tác với Trường 
+                                    - ngôi trường có bề dày lịch sử, có sự đào tạo chuyên sâu về khối ngành kĩ thuật và xây dựng. Bên cạnh đó, Ông cũng đã 
+                                    giới thiệu về công ty với những lĩnh vực hoạt động nổi trội, rất phù hợp với những ngành nghề CNTT  đào tạo ở Đại học Thủy lợi.
+                                </p>
+                                <p align="justify">
+                                    Chương trình hợp tác đào tạo giữa Khoa CNTT-ĐHTL và công ty Fsoft nhằm trang bị các kiến thức, các kỹ năng lập trình ứng dụng
+                                     đáp ứng yêu cầu của nhà tuyển dụng. Đây là chương trình rất hữu ích cho sinh viên, các bạn được trang bị cơ bản và nâng cao về SQL, 
+                                     lập trình ứng dụng với các ngôn ngữ C++/Win32, .Net và Java. Sinh viên sẽ có cơ hội được học tập và thực hành với những kiến thức 
+                                     thực tế nhất do giảng viên của FSOFT đảm nhận. Mỗi khóa đào tạo sẽ kéo dài 320 giờ cho sinh viên năm cuối của khoa CNTT. Sau khóa học này, 
+                                     các bạn sinh viên sẽ được cấp chứng nhận của FSOFT và có nhiều cơ hội được phỏng vấn vào làm việc tại FSOFT. Đây là một cơ hội rất tốt cho 
+                                     sinh viên được thử sức, trải nghiệm và làm quen với một môi trường làm việc hàng đầu và chuyên nghiệp tại Việt Nam.
+                                </p>
+                               <img src="../../images/cntt ky ket 1.jpg" alt="">
+                                <p align="justify">
+                                    Lễ ký kết  là mốc son đánh dấu quá trình hợp tác tốt đẹp và lâu dài giữa trường Đại học Thủy Lợi và Công ty FSOFT. Dưới sự chứng kiến của đại diện 
+                                    Ban lãnh đạo Nhà trường, khoa CNTT, các thầy, cô giáo và các bạn Sinh viên, TS Nguyễn Thanh Tùng - Trưởng khoa CNTT và Ông Trần Xuân Khôi -
+                                     Phó tổng giám đốc công ty FSOFT đã lên kí hợp đồng hợp tác. 
+                                </p>
+                                <p align="justify">
+                                    Việc liên kết hợp tác đào tạo,  mang đến cho sinh viên trường Đại học Thủy Lợi những nền tảng tốt , mở ra bước ngoặt và là giấy thông hành tạo lợi thế
+                                     cạnh tranh giúp sinh viên nhà trường ngay sau khi tốt nghiệp dễ dàng ứng tuyển, đảm nhận các vị trí quan trọng tại các doanh nghiệp chuyên về CNTT.
+                                </p>
+                                <p>Thông tin về khóa học:</p>
+                                 <p align="justify">
+                                     1)            Đối tượng: Tất cả các sinh viên K53, K54 và một số sinh viên có thành tích học tập tốt (điểm TB tích lũy >=2.3) của K55-Khoa CNTT.
+                                 </p>
+                                 <p align="justify">
+                                      2) Quyền lợi: <br> 
+                                        -      Học phí: Sinh viên không phải đóng bất kỳ khoản phí nào liên quan đến đào tạo. <br> <br>
+                                        -      Cơ hội việc làm: Sau khóa học, Fsoft tuyển dụng làm nhân viên thực tập với các sinh viên vượt qua buổi sát hạch (tương lai sẽ làm việc tại Fsoft – 
+                                        tham gia các dự án lớn trong nươc và dự án nước ngoài). Với các sinh viên không muốn làm việc tại Fsoft, các bạn vẫn có cơ hội cao về việc làm đối với 
+                                        doanh nghiệp khác khi được trang bị những kỹ năng lập trình ứng dụng hữu ích trên. <br> <br>
+                                        -      Nghĩa vụ: Sinh viên theo học phải làm bản cam kết với Khoa tham gia đầy đủ các buổi học lý thuyết và thực hành. <br> <br>
+                                        3) Tóm tắt về chương trình học: <br> 
+                                        -  &emsp;    Thời gian: Tổng thời gian 40 * 8 = 320 giờ học tại Nhà C5-Trung Tâm Thực Hành trường Đại học Thủy Lợi, có thể bố trí vào buổi tối và thứ 7, chủ nhật. <br> <br>
+                                        - &emsp;     Chỉ tiêu: Tuyển sinh 02 lớp, mỗi lớp tối đa 40 sinh viên <br> <br>
+                                        4)Các môn học: <br> 
+                                       ·   Know-How: 13 ngày <br> 
+                                        -   &emsp;   Quy trình làm việc <br> 
+                                        -   &emsp;   SQL Cơ bản <br> 
+                                        -   &emsp;   SQL Nâng cao       <br>                                      
+                                        ·   Industry Readiness: 27 ngày <br> 
+                                            - &emsp;      Kỹ năng lập trình (Basic and OOP): 12 ngày – Lựa chọn 1 trong 3 ngôn ngữ (C++, Java, .Net). <br>
+                                            -  &emsp;    Thực hành dự án: 15 ngày <br> 
+                                        ·   Fsoft Training Program: 15 ngày à 4 tháng <br> 
+                                            -  &emsp;    Sinh viên sau khóa học đạt chuẩn sẽ được thực tập tại công ty Fsoft <br> 
+                                            -&emsp;      Training về văn hóa, quy trình làm việc <br> 
+                                            -&emsp;      Có thể tham gia các dự án thực tế của công ty. <br> 
+                                            Thực hiện: Nguyễn Huế, Thành Đạt 
+                                 </p>
+                            </div>
+                             <div class="row" style="float: right;">
+                                <div class="col-md-12" style="float: right;">
+                                    <a href="../gioithieu/hoptacquocte.php">Trở về đầu trang</a>
+                                </div>
+                            </div>
+                            <div class="row icon" >
+                                <div class="col-md-3" style="margin-right: 4%;">
+                                    <a href=""> <button class="text-white" style="background-color: #2e5383; border: 0px;" ><i class="fab fa-facebook-f">Facebook</i></button></a>
+                                </div>
+                                <div class="col-md-3 " style="margin-right: 1%;">
+                                    <a href="" > <button class="bg-primary text-white" style="border: 0px;"><i class="fab fa-twitter">Twitter</i></button></a>
+                                </div>
+                                <div class="col-md-4">
+                                    <a href=""> <button style="background-color: #3161a0; border: 0px;" class="text-white"><i class="fab fa-linkedin-in">LinkIn</i></button></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 gioithieu">
+                            <div class="col-md-12" style="padding-bottom: 15%;">
+                                <h4><a href="../gioithieu/gioithieu.php">Giới thiệu</a></h4>
+                             </div>
+                            <div class="col-md-12 gioithieu1" >
+                                > <a href="../gioithieu/logokhoa.php" class="gioithieu2">   Logo Khoa CNTT</a>
+                            </div>
+
+                            <div class="col-md-12 gioithieu1">
+                                > <a href="../gioithieu/loichaomung.php" class="gioithieu2">Lời chào mừng </a>
+                            </div>
+                             <div class="col-md-12 gioithieu1">
+                            > <a href="../gioithieu/cocautochuc.php" class="gioithieu2">Tổ chức</a>
+                            </div>
+                            <div class="col-md-12 gioithieu1">
+                             > <a href="../gioithieu/hoptacquocte.php" class=gioithieu2>Hợp tác liên kết</a>
+                            </div>
+                        
+                        </div>
+                    </div>
+                   
+                </div>
+               
+                            
+            </div>
+         <footer>
+                <div class="container" style="max-width: 960px;">
+                  <div class="footer-conten">
+                    <div class="footer-top">
+                      <div class="it-nav-footer">
+                        <ul class="nav-footer-link">
+                          <li><a class="ui-link-white" href="../gioithieu/gioithieu.php">Giới thiệu</a></li>
+                          <li><a class="ui-link-white" href="../gioithieu/logokhoa.php">Logo của Khoa CNTT</a></li>
+                          <li><a class="ui-link-white" href="../gioithieu/loichaomung.php">Lời chào mừng</a></li>
+                          <li><a class="ui-link-white" href="../gioithieu/cocautochuc.php">Tổ chức</a></li>
+                          <li><a class="ui-link-white" href="../gioithieu/hoptacquocte.php">Hợp tác liên kết</a></li>
+                        </ul>
+                        <ul class="nav-footer-link">
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/dao-tao">Đào tạo</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/mo-hinh-dao-tao">Mô hình đào tạo</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/dao-tao-dai-hoc-chinh-quy">Đào tạo đại học</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/dao-tao-tien-si">Định hướng ngành nghề</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/dao-tao-sau-dai-hoc">Đào tạo sau đại học</a></li>
+                        </ul>
+                        <ul class="nav-footer-link">
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/khoa-hoc-cong-nghe">Nghiên cứu khoa học</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/thong-tin-seminar">Thông tin seminar</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/cac-de-tai-du-an">Các đề tài, dự án</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/cac-cong-trinh-cong-bo">Công trình công bố</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/cac-phong-thi-nghiem">Các phòng thí nghiệm</a></li>
+                        </ul>
+                        <ul class="nav-footer-link">
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/cong-nghe-phan-mem">Công nghệ phần mềm</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/he-thong-thong-tin">Hệ thống thông tin</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/khoa-hoc-may-tinh">Khoa học máy tính</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/ky-thuat-may-tinh-va-mang">Kỹ thuật máy tính và mạng</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/bo-mon-toan-hoc">Toán học</a></li>
+                          <li><a class="ui-link-white" href="http://cse.tlu.edu.vn/trung-tam-tin-hoc">Trung tâm học</a></li>
+                        </ul>
+    
+                      </div>
+                    </div>
+                    <div class="footer-bot" >
+                      <img src="../3.PROJECT/images/footer-img.png" alt="image" class="footer-bot-img">
+                      <p>
+                        © 2017 Khoa Công nghệ thông tin - Đại học Thủy lợi
+                        <br>
+                        
+                        Địa chỉ: nhà C1, Đại học Thủy lợi, 175 Tây Sơn, Đống Đa, Hà Nội. Điện thoại: (+84)-024 3 5632211. Email: 
+                        <a class="ui-link-white"  style="text-decoration: none; color: white;" href="http://cse.tlu.edu.vn/dao-tao/de-cuong-mon-hoc#">vpkcntt@tlu.edu.vn</a>
+                      </p>
+                    </div>               
+                  </div>
+                  
+                </div>
+              </footer>
+    </form>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+     <script src="https://kit.fontawesome.com/3ff58b9e8a.js" crossorigin="anonymous"></script>
+    </body>
+</html>
